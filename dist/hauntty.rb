063623d9d21a747de92bcf50b752b1a706651a9d
winterweken <admin@inscrip.com>
2026-09-05 10:38:28 -0400

    Release v1.0.0

--- dist/hauntty.rb
+++ dist/hauntty.rb
@@ -7,7 +7,7 @@
 class Hauntty < Formula
   desc "TUI theme & settings manager for the Ghostty terminal"
   homepage "https://github.com/winterweken/hauntty"
-  version "0.1.3"
+  version "1.0.0"
   license "MIT"
 
   on_macos do
