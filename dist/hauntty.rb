e7f252900ea751d94bef72f3098e8e68c0fc2c2b
winterweken <admin@inscrip.com>
2026-09-05 10:40:43 -0400

    Update Homebrew formula for v1.0.0

--- dist/hauntty.rb
+++ dist/hauntty.rb
@@ -13,22 +13,22 @@ class Hauntty < Formula
   on_macos do
     on_arm do
       url "https://github.com/winterweken/hauntty/releases/download/v#{version}/hauntty-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "5e8d248366cef4467f9dfe2d9dfcb0276e389c8df5b444499f4e32413d241e09"
+      sha256 "7c85a1b7f7425430a6d7fa4fc7aaa40f2b6ecba861278b563d0fa722ee06b231"
     end
     on_intel do
       url "https://github.com/winterweken/hauntty/releases/download/v#{version}/hauntty-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "0e803955862cc5b3ab26b0d042d07e7a9ac6beddbee3182013c4a26e14a8b66d"
+      sha256 "5a93bfeacffcef1f572c2082f5046ffe5a1363b491aa572e9262b63d9a839360"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/winterweken/hauntty/releases/download/v#{version}/hauntty-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "fcea1490fde626f8f6abaa6cfd82105a86e9f1e78ce02cc399a2d8275c9836e0"
+      sha256 "69b992cc260d0a12dde84434dae1843ddfa7df08ab12e115ba1de263475fa59c"
     end
     on_intel do
       url "https://github.com/winterweken/hauntty/releases/download/v#{version}/hauntty-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "7a7d817cebefafd8e892b1d8f01e70452aca64d659498a850fa53da3308eb20c"
+      sha256 "26e0e06fd15cd16cf7d3c35ff31c663d2ec7416a1cc0af4b94a9db82ec59b736"
     end
   end
 
